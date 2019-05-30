18b82f51cb26e16c4c2ef4de3ff1f188a4037831
Dirt Deodara <thecrowleaps@gmail.com>
2019-05-30 14:00:10 -0700

    build out Header compnent

---
+++ components/App.js
@@ -0,0 +1,29 @@
+import Component from './Component.js';
+import Header from './Header.js';
+
+class App extends Component {
+    render() {
+
+        const dom = this.renderDOM();
+
+        const header = new Header();
+        const headerDOM = header.render();
+
+        const main = dom.querySelector('main');
+        dom.insertBefore(headerDOM, main);
+
+        return dom;
+
+    }
+
+
+    renderTemplate() {
+        return `
+            <div>
+                <main></main>
+            </div>
+        `;
+    }
+}
+
+export default App;
---
+++ components/Header.js
@@ -0,0 +1,14 @@
+import Component from './Component.js';
+
+class Header extends Component {
+
+
+    renderTemplate() {
+        return `
+            <header>
+                <h1>Pokedex</h1>
+            </header>
+        `;
+    }
+}
+export default Header;
